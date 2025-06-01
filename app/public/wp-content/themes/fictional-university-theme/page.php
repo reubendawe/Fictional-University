cb9f41a6a21074a5b9e1adc6bf8254a8b16bdfff
Reuben <reubendawemarketing@gmail.com>
2025-06-01 13:40:57 +0100

    Code fixes

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -6,6 +6,9 @@ while(have_posts()) {
     <h1>This is a page, not a post.</h1>
     <h2><?php the_title(); ?></h2>
     <?php the_content(); ?>
+    <?php if (wp_get_post_parent_id(get_the_ID())) {
+        echo "I am a child page.";
+    } ?>
     <?php }
 
 
