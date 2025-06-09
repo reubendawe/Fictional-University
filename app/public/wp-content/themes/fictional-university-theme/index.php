12bc58ea583a969d9242419d7e1e5a4f139d1f0a
Reuben <reubendawemarketing@gmail.com>
2025-06-09 17:07:45 +0100

    Lesson complete - The Famous 'Loop' in Wordpress

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,12 +1,10 @@
-<?php get_header();
+<?php 
 
-//Blog posts
 while(have_posts()) {
     the_post(); ?>
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
+    <h2><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h2>
     <?php the_content(); ?>
     <hr>
-    <?php }
+<?php }
 
-get_footer();
 ?>
