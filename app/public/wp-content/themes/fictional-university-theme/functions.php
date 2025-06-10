9a9538b034566559ea88845fe37cbddda28a89b2
Reuben <reubendawemarketing@gmail.com>
2025-06-10 13:21:54 +0100

    Functions.php pulling in CSS file

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -1,5 +1,9 @@
 <?php
 
-add_action('wp_enqueue_scripts', '');
+function university_files() {
+    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'university_files');
 
 ?>
--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php wp_head(); ?>
+    <?php wp_head(); ?> <!-- Loads in CSS -->
 </head>
 <body>
     <h1>Fictional University</h1>
