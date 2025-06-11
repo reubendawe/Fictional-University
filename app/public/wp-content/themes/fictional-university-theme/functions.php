db2c9ecdbb6bde589f866c2cd8a36a34421db446
Reuben <reubendawemarketing@gmail.com>
2025-06-11 17:36:13 +0100

    Further elaboration on Functions page

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -8,6 +8,8 @@ function university_files() {
 
 // Calling in the function, the second argument is the name of the function that I want WP
 // to call at a specific moment. The first argument is how I say which moment that will be.
+// It's basically saying "Right, Wordpress right before you output the code that's going to 
+// go in the header, the wp head area, I want to tack onto that moment and run my custom function
 add_action('wp_enqueue_scripts', 'university_files');
 
 ?>
