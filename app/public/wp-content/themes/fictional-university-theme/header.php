a41324472fc7fd7c1bd1163e0f9207889c3e2744
Reuben <reubendawemarketing@gmail.com>
2025-06-11 17:43:09 +0100

    Black admin bar added to site

--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -5,5 +5,3 @@
 </head>
 <body>
     <h1>Fictional University</h1>
-</body>
-</html>
--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,5 +1,6 @@
 <?php 
 
+// Outputting main content of the page
 while(have_posts()) {
     the_post(); ?>
     <h2><a href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h2>
